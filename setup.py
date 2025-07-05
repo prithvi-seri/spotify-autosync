3d5c54788e908217e46cca04a865368e4f8e4e6d
Prithvi Seri <prithvi.seri@gmail.com>
2025-07-05 02:05:10 -0400

    Moved .storage creation into Storage class (on initialization)

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from auth import get_auth_code, get_access_token_with_auth_code
-from pathlib import Path
+from storage import initalize_storage
 
-Path('.storage').mkdir(exist_ok=True)
 get_auth_code()
 auth_code = input('Paste code from URL:\n>> ')
 get_access_token_with_auth_code(auth_code)
--- storage.py
+++ storage.py
@@ -5,6 +5,8 @@ class Storage:
   def __init__(self, filename: str):
     self.file = Path(STORAGE_DIR) / filename
     self.file.write_text('')  # Clear file and create if necessary
+    storage_dir = Path(STORAGE_DIR)
+    if not storage_dir.exists(): storage_dir.mkdir(exist_ok=True)
 
 def store(self, data: str):
   return self.file.write_text(data)
