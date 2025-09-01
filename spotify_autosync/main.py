b8bd007b7931f2176dd89ae210434ef290891604
Prithvi Seri <prithvi.seri@gmail.com>
2025-08-31 22:08:50 -0400

    Updated Lambda handler

--- spotify_autosync/main.py
+++ spotify_autosync/main.py
@@ -1,11 +1,11 @@
 from .playlists import update_playlists, trim_new_playlist
 
 def handler(event, context):
-  if event.get('source') == 'aws.events':
+  if event and event.get('task') == 'trim':
     trim_new_playlist()
   else:
     update_playlists()
   return 'Success!'
 
 if __name__ == '__main__':
-  handler({}, {})
+  handler(None, None)
